20bf8e442754c30b76216d48a5047820afe6d63e
chanaka_sampath <Chanakasampathrulez@gmail.com>
2025-06-28 00:24:07 +0530

    site fixes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SearchController;
 use App\Models\Ad;
 use App\Models\Category;
@@ -9,30 +10,16 @@ use Livewire\Volt\Volt;
 use App\Http\Controllers\CategoryController;
 
 
+Route::middleware('web')->group(function () {
 
-Route::get('/', function () {
-    $categories = Category::all();
-    return view('welcome', compact('categories'));
-});
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/search', [SearchController::class, 'search']);
+    Route::get('/search', [SearchController::class, 'search'])->name('search');
 
-Route::get('/ads/create', [AdController::class, 'create'])->name('ads.create');
-Route::post('/ads', [AdController::class, 'store'])->name('ads.store');
+    Route::get('/ads/create', [AdController::class, 'create'])->name('ads.create');
+    Route::post('/ads', [AdController::class, 'store'])->name('ads.store');
 
-
-
-Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('categories.show');
-
-Route::get('/test-thumb', function () {
-    $ad = Ad::find(1);
-    $media = $ad->getFirstMedia('ads');
-
-    dd([
-        'original' => $media?->getUrl(),
-        'thumb'    => $media?->getUrl('thumb'),
-        'exists'   => $media?->hasGeneratedConversion('thumb'),
-    ]);
+    Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('categories.show');
 });
 
 
